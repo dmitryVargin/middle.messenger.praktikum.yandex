f45f19505d9b73dd75b86d9c24b77e75ec41dd19
vargin <vargin@mkb.ru>
2021-08-29 20:46:42 +0300

    Try delete host

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const host = '0.0.0.0';
 const port = 3000;
 app.use(express.static('dist'));
 app.get(/.*/, (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 });
-app.listen(port, host);
+app.listen(port);
