e1224f9241eb7c92e01316b9a7bf0f35ca311e00
Dmitry Vargin <davrgin@mail.com>
2021-08-29 15:51:50 +0300

    Docker added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 app.get(/.*/, (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 });
-app.listen(3000);
+app.listen(port,host);
